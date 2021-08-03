5c3b838851bf9b96ad93c61b7e666624f2ecd8e0
Eric McCullough <mccullougheric96@gmail.com>
2021-08-03 16:07:27 -0500

    BPA-4: Refactor to function component

--- src/components/envelopeView/envelopeView.js
+++ src/components/envelopeView/envelopeView.js
@@ -1,15 +1,11 @@
-import { Component } from 'react';
-
-export class EnvelopeView extends Component {
-    render() {
-        return(
-            <div className='envelope-view'>
-                <h1>User Envelopes</h1>
-                <ul>
-                    <li>Food: $1,500.00</li>
-                    <li>Rent: $2,000.00</li>
-                </ul>
-            </div>
-        );
-    }
+export function EnvelopeView() {
+  return (
+    <div className='envelope-view'>
+      <h1>User Envelopes</h1>
+      <ul>
+        <li>Food: $1,500.00</li>
+        <li>Rent: $2,000.00</li>
+      </ul>
+    </div>
+  );
 }
